027b8dfe24f9f68be2a3d9250d831aacacd04471
Emil Kriegel <emilkriegel@gmail.com>
2025-09-02 21:29:32 +0200

    Third commit, populater done

--- src/test/java/app/daos/DolphinDAOTest.java
+++ src/test/java/app/daos/DolphinDAOTest.java
@@ -1,17 +1,48 @@
 package app.daos;
 
+import app.config.HibernateConfig;
+import app.entities.Person;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import populators.PersonPopulator;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 class DolphinDAOTest {
 
+    private static final EntityManagerFactory emf = HibernateConfig.getEntityManagerFactoryForTest();
+    private static final DolphinDAO testDao = new DolphinDAO(emf);
+    private static Person p1;
+    private static Person p2;
+
     @BeforeEach
     void setUp() {
+        try (EntityManager em = emf.createEntityManager()) {
+            em.getTransaction().begin();
+            em.createQuery("DELETE FROM Person").executeUpdate();
+            em.createNativeQuery("ALTER person_id_seq RESTART WITH 1");
+            em.getTransaction().commit();
+            Person[] persons = PersonPopulator.populate(testDao);
+            p1 = persons[0];
+            p2 = persons[1];
+        }catch(Exception e){
+            e.printStackTrace();
+        }
     }
 
     @Test
     void create() {
+        try (EntityManager em = emf.createEntityManager()) {
+            em.getTransaction().begin();
+            em.persist(p1);
+            List<Person> persons = testDao.getAll();
+            assertEquals(1, persons.size());
+        }catch(Exception e){
+            e.printStackTrace();
+        }
     }
 
     @Test
--- src/test/java/populators/PersonPopulator.java
+++ src/test/java/populators/PersonPopulator.java
@@ -1,5 +1,44 @@
 package populators;
 
+import app.daos.DolphinDAO;
+import app.entities.Fee;
+import app.entities.Person;
+import app.entities.PersonDetail;
+
+import java.time.LocalDate;
+
 public class PersonPopulator {
-    public
+    public static Person[] populate (DolphinDAO dao){
+        Fee f1 = Fee.builder().amount(125).payDate(LocalDate.of(2023, 8, 25)).build();
+        Fee f2 = Fee.builder().amount(150).payDate(LocalDate.of(2023, 7, 19)).build();
+
+
+        Person p1 = Person.builder()
+                .name("Bo")
+                .build();
+
+        PersonDetail pd1 = PersonDetail.builder()
+                .person(p1)
+                .zip(3050)
+                .city("Humlebæk")
+                .Address("Baunebjergvej")
+                .age(65)
+                .build();
+        p1.addPersonDetail(pd1);
+
+        Person p2 = Person.builder()
+                .name("Emil")
+                .build();
+
+        PersonDetail pd2 = PersonDetail.builder()
+                .person(p2)
+                .zip(2860)
+                .city("Søborg")
+                .Address("Tobaksvejen")
+                .age(30)
+                .build();
+        p2.addPersonDetail(pd2);
+
+        return new Person[]{p1, p2};
+    }
 }
